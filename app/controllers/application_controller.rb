d92410d98283274eaab1c0eef0f97eab85b0ac16
Tiffeney <tiffeney.rambo@gmail.com>
2018-09-17 12:22:02 -0700

    Added Navbar and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,17 @@ class ApplicationController < ActionController::Base
 
     private
 
-    helper_method :current_user
-
     def current_user
-        @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-      end
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
 
-      def authorize
-        redirect_to login_path, alert: 'Not authorized - you must be logged in!' if current_user.nil?
-      end
+    def logged_in?
+      !!current_user
+    end
+
+    def authorize
+      redirect_to login_path, alert: 'Not authorized - you must be logged in!' if current_user.nil?
+    end
+
+    helper_method :current_user, :logged_in?
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   get '/login', to: 'sessions#new'
   get '/signup' => 'users#new'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
